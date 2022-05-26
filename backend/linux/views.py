c3aef386875c77c10101a00e271f555ecb756334
Yusuf Khasbulatov <khashashin@gmail.com>
2022-05-26 19:22:15 +0200

    feat(backend-auth): Add django-allauth package

--- backend/linux/views.py
+++ backend/linux/views.py
@@ -8,7 +8,8 @@ class IndexView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         # Redirect to login page if not logged in
         if not request.user.is_authenticated:
-            return redirect('/login')
+            return redirect(f'/accounts/login/?next={request.path}')
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
